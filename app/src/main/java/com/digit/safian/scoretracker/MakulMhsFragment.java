d2c8f9f4920023bbf0a9fffc1298f42e95ad180b
Faqih Arifian <faqiharifianaji@gmail.com>
2015-04-26 15:36:40 +0700

    added bulkinsert in fragment though it has errors

--- app/src/main/java/com/digit/safian/scoretracker/MakulMhsFragment.java
+++ app/src/main/java/com/digit/safian/scoretracker/MakulMhsFragment.java
@@ -1,5 +1,7 @@
 package com.digit.safian.scoretracker;
 
+import android.content.ContentValues;
+import android.content.Context;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -13,6 +15,8 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import com.digit.safian.scoretracker.data.ScoreContract;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -24,6 +28,7 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Vector;
 
 public class MakulMhsFragment extends Fragment {
 
@@ -57,7 +62,7 @@ public class MakulMhsFragment extends Fragment {
     }
 
     private void updateMakulMhs(){
-        FetchMakulMhsTask makulTask = new FetchMakulMhsTask();
+        FetchMakulMhsTask makulTask = new FetchMakulMhsTask(getActivity());
         makulTask.execute();
     }
 
@@ -68,15 +73,19 @@ public class MakulMhsFragment extends Fragment {
     }
 
 
-    public class FetchMakulMhsTask extends AsyncTask<Void, Void, String[]> {
+    public class FetchMakulMhsTask extends AsyncTask<Void, Void, Void> {
 
+        private final Context mContext;
+        public FetchMakulMhsTask(Context context){
+            mContext = context;
+        }
         private final String LOG_TAG = FetchMakulMhsTask.class.getSimpleName();
 
         private String formatName(String name){
             return name.replace("name: ","");
         }
 
-        private String[] getMakulDataFromJson(String makulJsonStr)
+        private void getMakulDataFromJson(String makulJsonStr)
                 throws JSONException {
             // These are the names of the JSON objects that need to be extracted.
             final String OWM_RESPONSEDATA = "responseData";
@@ -89,6 +98,7 @@ public class MakulMhsFragment extends Fragment {
             JSONObject responseFeed = jsonResponse.getJSONObject(OWM_FEED);
             JSONArray makulArray = responseFeed.getJSONArray(OWM_ENTRIES);
 
+            Vector<ContentValues> cVVector = new Vector<ContentValues>(makulArray.length());
             String[] resultStrs = new String[makulArray.length()];
             for(int i = 0; i < makulArray.length(); i++) {
                 String content;
@@ -97,18 +107,32 @@ public class MakulMhsFragment extends Fragment {
                 JSONObject iMakul = makulArray.getJSONObject(i);
                 content = iMakul.getString(OWM_CONTENT);
 
-                resultStrs[i] = formatName(content);
-            }
 
+                resultStrs[i] = formatName(content);
+
+                ContentValues makulValues = new ContentValues();
+
+                makulValues.put(ScoreContract.MakulEntry.COLUMN_NAMA_MAKUL, resultStrs[i]);
+
+                cVVector.add(makulValues);
+
+            }
+            if (cVVector.size() > 0) {
+                ContentValues[] cvArray = new ContentValues[cVVector.size()];
+                cVVector.toArray(cvArray);
+                mContext.getContentResolver().bulkInsert(ScoreContract.MakulEntry.CONTENT_URI, cvArray);
+
+            }
             for (String s : resultStrs) {
                 Log.v(LOG_TAG, "Forecast entry: " + s);
             }
-            return resultStrs;
+
+            //return resultStrs;
 
         }
 
         @Override
-        protected String[] doInBackground(Void... params){
+        protected Void doInBackground(Void... params){
             // These two need to be declared outside the try/catch
             // so that they can be closed in the finally block.
             HttpURLConnection urlConnection = null;
@@ -170,7 +194,8 @@ public class MakulMhsFragment extends Fragment {
                 }
             }
             try{
-                return getMakulDataFromJson(makulJsonStr);
+                getMakulDataFromJson(makulJsonStr);
+                //return getMakulDataFromJson(makulJsonStr);
             }catch(JSONException e){
                 Log.e(LOG_TAG, e.getMessage(), e);
                 e.printStackTrace();
@@ -178,7 +203,7 @@ public class MakulMhsFragment extends Fragment {
             return null;
         }
 
-        @Override
+        /*@Override
         protected void onPostExecute(String[] result) {
             if (result != null){
                 mMakulAdapter.clear();
@@ -186,7 +211,7 @@ public class MakulMhsFragment extends Fragment {
                     mMakulAdapter.add(dayForecastStr);
                 }
             }
-        }
+        }*/
     }
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
