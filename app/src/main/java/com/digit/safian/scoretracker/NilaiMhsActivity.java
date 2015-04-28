f90679c8b439fac597edc97cb0f21983a5391b3b
Faqih Arifian <faqiharifianaji@gmail.com>
2015-04-28 14:37:06 +0700

    trying to make fixed row and column header

--- app/src/main/java/com/digit/safian/scoretracker/NilaiMhsActivity.java
+++ app/src/main/java/com/digit/safian/scoretracker/NilaiMhsActivity.java
@@ -1,13 +1,19 @@
 package com.digit.safian.scoretracker;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
+import android.widget.TableRow.LayoutParams;
 
 
 public class NilaiMhsActivity extends ActionBarActivity {
@@ -21,8 +27,58 @@ public class NilaiMhsActivity extends ActionBarActivity {
                     .add(R.id.container, new PlaceholderFragment())
                     .commit();
         }
+
+        TableRow.LayoutParams wrapWrapTableRowParams = new TableRow.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+        int[] fixedColumnWidths = new int[]{20, 20, 20, 20, 20};
+        int[] scrollableColumnWidths = new int[]{20, 20, 20, 30, 30};
+        int fixedRowHeight = 50;
+        int fixedHeaderHeight = 60;
+
+        TableRow row = new TableRow(this);
+        //header (fixed vertically)
+        TableLayout header = (TableLayout) findViewById(R.id.table_header);
+        row.setLayoutParams(wrapWrapTableRowParams);
+        row.setGravity(Gravity.CENTER);
+        row.setBackgroundColor(Color.YELLOW);
+        row.addView(makeTableRowWithText("col 1", fixedColumnWidths[0], fixedHeaderHeight));
+        row.addView(makeTableRowWithText("col 2", fixedColumnWidths[1], fixedHeaderHeight));
+        row.addView(makeTableRowWithText("col 3", fixedColumnWidths[2], fixedHeaderHeight));
+        row.addView(makeTableRowWithText("col 4", fixedColumnWidths[3], fixedHeaderHeight));
+        row.addView(makeTableRowWithText("col 5", fixedColumnWidths[4], fixedHeaderHeight));
+        header.addView(row);
+        //header (fixed horizontally)
+        TableLayout fixedColumn = (TableLayout) findViewById(R.id.fixed_column);
+        //rest of the table (within a scroll view)
+        TableLayout scrollablePart = (TableLayout) findViewById(R.id.scrollable_part);
+        for(int i = 0; i < 10; i++) {
+            TextView fixedView = makeTableRowWithText("row number " + i, scrollableColumnWidths[0], fixedRowHeight);
+            fixedView.setBackgroundColor(Color.BLUE);
+            fixedColumn.addView(fixedView);
+            row = new TableRow(this);
+            row.setLayoutParams(wrapWrapTableRowParams);
+            row.setGravity(Gravity.CENTER);
+            row.setBackgroundColor(Color.WHITE);
+            row.addView(makeTableRowWithText("value 2", scrollableColumnWidths[1], fixedRowHeight));
+            row.addView(makeTableRowWithText("value 3", scrollableColumnWidths[2], fixedRowHeight));
+            row.addView(makeTableRowWithText("value 4", scrollableColumnWidths[3], fixedRowHeight));
+            row.addView(makeTableRowWithText("value 5", scrollableColumnWidths[4], fixedRowHeight));
+            scrollablePart.addView(row);
+        }
     }
 
+    //util method
+    private TextView recyclableTextView;
+
+    public TextView makeTableRowWithText(String text, int widthInPercentOfScreenWidth, int fixedHeightInPixels) {
+        int screenWidth = getResources().getDisplayMetrics().widthPixels;
+        recyclableTextView = new TextView(this);
+        recyclableTextView.setText(text);
+        recyclableTextView.setTextColor(Color.BLACK);
+        recyclableTextView.setTextSize(20);
+        recyclableTextView.setWidth(widthInPercentOfScreenWidth * screenWidth / 100);
+        recyclableTextView.setHeight(fixedHeightInPixels);
+        return recyclableTextView;
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
