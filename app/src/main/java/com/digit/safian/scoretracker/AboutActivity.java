0914bde1de16e8e2632cc16223ce1c0a9e885c3f
Faqih Arifian <faqiharifianaji@gmail.com>
2015-05-28 23:23:53 +0700

    applying what reviewer suggesting

--- app/src/main/java/com/digit/safian/scoretracker/AboutActivity.java
+++ app/src/main/java/com/digit/safian/scoretracker/AboutActivity.java
@@ -11,20 +11,6 @@ public class AboutActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
 
-        /*final TextView myEmail = (TextView) findViewById(R.id.myEmail);
-        myEmail.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(Intent.ACTION_SEND);
-                i.setType("message/rfc822");
-                i.putExtra(Intent.EXTRA_EMAIL  , new String[]{myEmail.getText().toString()});
-                try {
-                    startActivity(Intent.createChooser(i, "Send mail..."));
-                } catch (android.content.ActivityNotFoundException ex) {
-                    Toast.makeText(AboutActivity.this, "There are no email clients installed.", Toast.LENGTH_SHORT).show();
-                }
-            }
-        });*/
     }
 
 }
